2f0bfc3b59dfb96b703a2e61344d4499a666190f
collin <cstewa91@gmail.com>
2020-09-01 00:07:39 -0700

    commiting

--- client/src/components/messages/messages.js
+++ client/src/components/messages/messages.js
@@ -2,10 +2,11 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
-import { getUserInfo } from '../../actions'
-import { getFriends } from '../../actions'
+import { animateScroll } from 'react-scroll';
+import { getUserInfo } from '../../actions';
+import { getFriends } from '../../actions';
 import './messages.scss';
-import firebase from '../../firebase'
+import firebase from '../../firebase';
 
 
 class Messages extends Component {
@@ -13,6 +14,8 @@ class Messages extends Component {
     state = {
         messages: [],
         allFriends: [],
+        currentFriend: {},
+        prevMessage: {},
     }
 
     componentDidMount = async () => {
@@ -22,6 +25,17 @@ class Messages extends Component {
         this.getMessages()
     }
 
+    componentDidUpdate = () => {
+        this.scrollToBottom();
+    }
+
+    scrollToBottom = () => {
+        animateScroll.scrollToBottom({
+            containerId: "messages",
+            duration: 0
+        });
+    }
+
     setFriendsToState = () => {
         this.props.friends.map(async (friend, i) => {
             let userData = await this.props.getUserInfo(friend.account)
@@ -29,9 +43,15 @@ class Messages extends Component {
             userObj.account = friend.account
             userObj.name = `${this.props.friendInfo.firstname} ${this.props.friendInfo.lastname}`
             userObj.picture = this.props.friendInfo.profilepicture
+            userObj.handler = this.props.friendInfo.handler
             this.setState(prevState => ({
                 allFriends: [...prevState.allFriends, userObj]
             }))
+            if(userObj.account == this.props.match.params.account) {
+                this.setState(prevState => ({
+                    currentFriend: userObj
+                }))
+            }
             this.getLastMessage(friend.account)
         })
     }
@@ -50,7 +70,7 @@ class Messages extends Component {
                         <Link to={`/messages/${friend.account}`}>
                                 <div className="profile-picture"><img src={this.profilePictureSrc(friend.picture)} alt=""/></div>
                                 <div className="info">
-                                    <p className="green-text">{friend.name}</p>
+                                    <p className="green-text friend-name">{friend.name}</p>
                                     <p>{friend.lastMessage}</p>
                                 </div>
                         </Link>
@@ -62,7 +82,7 @@ class Messages extends Component {
                         <Link to={`/messages/${friend.account}`}>
                                 <div className="profile-picture"><img src={this.profilePictureSrc(friend.picture)} alt=""/></div>
                                 <div className="info">
-                                    <p className="green-text">{friend.name}</p>
+                                    <p className="green-text friend-name">{friend.name}</p>
                                     <p>{friend.lastMessage}</p>
                                 </div>
                         </Link>
@@ -129,23 +149,68 @@ class Messages extends Component {
         });
     }
 
+    renderMessageBar = () => {
+        let friendName = ''
+        if(this.state.currentFriend.name) {
+            friendName = this.state.currentFriend.name.split(' ')
+        }
+        return (
+            <Fragment>
+                <Link to={`/profile/${this.state.currentFriend.account}`} className="message-bar-img"><div ><img src={this.profilePictureSrc(this.state.currentFriend.picture)} alt=""/></div></Link>
+                <Link to={`/profile/${this.state.currentFriend.account}`} className="message-bar-name "><p className="green-text">{friendName[0]}</p></Link>
+                <Link to={`/profile/${this.state.currentFriend.account}`} className="message-bar-handler"><p>@{this.state.currentFriend.handler}</p></Link>
+                <div className="message-bar-menu"></div>
+            </Fragment>
+        )
+    }
+
     renderMessages = () => {
-        const messages = this.state.messages.map((message, i) => {
+        const date = new Date()
+        const messages = this.state.messages.map((message, i, arr) => {
             let position = '';
-            message.account == this.props.user.account ? position = "right" : position = 'left'
-            return (
-                <div key={i} className={`message ${position}`} >
-                    <p>{message.name}</p>
-                    <p>{message.text}</p>
-                </div>
-            )
+            let messageImg = ''
+            let prevmessage = ''
+            let nextMessage = ''
+            console.log(message)
+            if(message.account == this.props.user.account) {
+                position = "right"
+                messageImg = this.profilePictureSrc(this.props.user.profilepicture)
+            } else {
+                position = 'left'
+                messageImg = this.profilePictureSrc(this.state.currentFriend.picture)
+            }
+            i > 0 ? prevmessage = arr[i - 1].account : prevmessage = ''
+            i < arr.length - 1 ? nextMessage = arr[i + 1].account : nextMessage = ''
+            if(message.account == prevmessage && message.account == nextMessage) {
+                return (
+                    <div key={i} className={`message ${position}`} >
+                        <p>{message.text}</p>
+                    </div>
+                )
+            } else if(message.account != prevmessage && message.account == nextMessage){
+
+            } else if(message.account == prevmessage && i == arr.length - 1) {
+                return (
+                    <div key={i} className={`message ${position}`} >
+                        <p>{message.text}</p>
+                        <div className="message-img"><img src={messageImg} alt=""/></div>
+                    </div>
+                )
+            } else {
+                return (
+                    <div key={i} className={`message ${position}`} >
+                        <p>{message.text}</p>
+                        <div className="message-img"><img src={messageImg} alt=""/></div>
+                    </div>
+                )
+            }
         });
 
         return messages;
     }
 
     saveMessage = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         let userAccount = this.props.user.account;
         let friendAccount = this.props.match.params.account
         let message = e.target[0].value
@@ -153,6 +218,7 @@ class Messages extends Component {
         if(friendAccount < userAccount) {
             query = firebase.firestore().collection(`${friendAccount} ${userAccount}`);
         }
+        e.target.reset();
         return query.add({
             account: userAccount,
             name: this.props.user.firstname,
@@ -174,11 +240,14 @@ class Messages extends Component {
                     </div>
                 </div>
                 <div className="message-container">
-                    <div className="messages">
+                    <div className="message-bar">
+                        {this.renderMessageBar()}
+                    </div>
+                    <div id="messages" className="messages">
                         {this.renderMessages()}
                     </div>
-                    <form onSubmit={this.saveMessage}>
-                        <input type="text" placeholder="Type Something..." />
+                    <form id="message-form" onSubmit={this.saveMessage}>
+                        <input type="text" name="newMessage" placeholder="Type Something..." />
                         <button className="btn">Send</button>
                     </form>
                 </div>
